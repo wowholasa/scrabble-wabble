2f2547f9bec8de0a0ddfccc5cdd9d5e5f90edbfd
Mads Nørklit Jensen <macj@itu.dk>
2024-04-17 13:31:42 +0200

    Added board things from template that we had removed on accident
    Added dictionary test in program.fs
    
    Co-authored-by: Max <mbko@itu.dk>

--- ScrabbleBot/Parser.fs
+++ ScrabbleBot/Parser.fs
@@ -115,4 +115,16 @@ module internal Parser
 
     let parseBoardProg _ = failwith "not implemented"
 
+     (* The rest of your parser goes here *)
+    type word   = (char * int) list
+    type squareFun = word -> int -> int -> Result<int, Error>
+    type square = Map<int, squareFun>
+    
+    type boardFun2 = coord -> Result<square option, Error>
+        
+    type board = {
+        center        : coord
+        defaultSquare : square
+        squares       : boardFun2
+    }
     let mkBoard (bp: boardProg) : board = failwith "not implemented"
