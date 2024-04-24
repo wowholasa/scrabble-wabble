f4ea881f6eb6dd17cc6084aa1fa78823aaf00702
Mads Nørklit Jensen <macj@itu.dk>
2024-04-24 14:42:49 +0200

    Made whole function/algorithm for placeFirstWord (not working yet)
    Still need to implement Multiset.ToList to make it work
    Maybe also Multiset.ofList ?
    
    Co-authored-by: Max <mbko@itu.dk>

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -37,7 +37,7 @@ let foldBack (f: 'a -> uint32 -> 'b -> 'b) (R s: MultiSet<'a>) (x: 'b) = Map.fol
 
 // Yellow exercises
 let ofList (lst: 'a list) : MultiSet<'a> = failwith ""
-let toList (R s: MultiSet<'a>) : 'a list = failwith ""
+let toList (R s: MultiSet<'a>) : 'a list = failwith "Hej Mads"
 
 
 let map (_: 'a -> 'b) (_: MultiSet<'a>) : MultiSet<'b> = failwith "" // R ()
